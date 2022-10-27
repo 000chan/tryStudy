57ce3ac6eadac58670b2dd4033a2c2c60e4b3f4b
이충재 <knm8643@nate.com>
2022-10-27 19:54:56 +0900

    [PGS] 프린터 / 아이언2 / 58분" -m "https://school.programmers.co.kr/learn/courses/30/lessons/42587"

--- L/lv2/프린터.js
+++ L/lv2/프린터.js
@@ -1,21 +1,35 @@
 function solution(priorities, location) {
     var answer = 0;
-    var one = priorities.slice(0,1);
+    var one = priorities[0];
 
     if(location != 0) {
-        for(var i = 0; i<location; i++){
-            if(location == priorities[i]){
-                answer = priorities[i+1];
+        for(var i=0; i<location; i++){
+            answer = priorities[i];
+        }
+        priorities.shift();
+        if(answer == priorities[0]){
+            priorities.shift();
+            priorities.push(answer);
+            priorities.push(one);
+        }
+        console.log(priorities);
+    }else {
+        for(var a=0; a<priorities.length-1; a++){
+            answer = priorities.length-1
+        }
+        if(priorities[0] == one){
+            priorities.push(one)
+            priorities.shift();
+            if(priorities[0]){
+                priorities.push(one);
+                priorities.shift();
             }
         }
-    } else {
-        for(var i=0; i<priorities.length; i++){
-            answer = i;
-        }
+        console.log(priorities);
     }
     return answer;
-}
-
+};
 
 // 음주 문제풀이 정답 리턴 값은 맞으나, 문제에서 제시한 요지에 벗어남
 // 다시풀자 2022-10-26
+// 두번째 시도 실패 2022-10-27
