313d3254f83b5104d0428bfed9c82e2dac3dc57e
이충재 <knm8643@nate.com>
2022-11-18 11:21:57 +0900

    [PGS] 로그인 성공(통과) / 아이언1 / 60분" -m "사이트 길어서 제외"

--- L/lv0/로그인 성공?.js
+++ L/lv0/로그인 성공?.js
@@ -13,37 +13,10 @@ function solution(id_pw, db) {
         }
 
         if(answer == '' && db[i][0] != userId){
-            console.log(db[i][0]);
-            if(db[i][1] != userPw){
-                console.log(db[i][1]);
+            if(db[i][1] == userPw){
+                answer = (db[i][0] != userId)?"fail":""
             }
         }
     }
-
-
-    // for(var i=0; i<db.length; i++){
-    //     if(userId == db[i][0]){
-    //         answer = (userPw!=db[i][1])?'wrong pw':'fail'
-    //         if(userPw == db[i][1]){
-    //            answer = 'login';
-    //         }
-    //     } else {
-    //         answer = 'fail'
-    //     }
-    // }
-
-    // for(var i=0; i<db.length; i++){
-    //     if(userId != db[i][0]){
-    //         answer = "fail";
-    //     } else if(userId == db[i][0]){
-    //         if(userPw == db[i][1]){
-    //             answer = 'login';
-    //         } else if(userPw != db[i][1]) {
-    //             answer = 'wrong pw';
-    //         }
-    //     }
-    // }
-
-    // 훈재때문에 집에가서 마무리 ㅂㄷㅂㄷ
     return answer;
 }
