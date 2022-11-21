fd6cd876c3fc3702a6fd78144674117634f5eadd
이충재 <knm8643@nate.com>
2022-11-21 20:30:25 +0900

    테스트

--- L/lv0/세균증식.java
+++ L/lv0/세균증식.java
@@ -1,12 +1,12 @@
 package lv0;
 
 public class 세균증식 {
-    public int solution(int n, int t){
-        int answer = 0;
-        answer = (int)Math.pow(2,t) * n;
-        return answer;
+//    public int solution(int n, int t){
+//        int answer = 0;
+//        answer = (int)Math.pow(2,t) * n;
+//        return answer;
 
         // math 기능을 몰라서 삽질한 시간이 많았음,
-        // 아는만큼 승리한다. 왜 나는 초록불 안뜨지
-    }
+        // 아는만큼 승리한다. 왜 나는 초록불 안뜨지 이제 초록불 뜨겠다 영찬이의 포크드에서 자유로워지다
+//    }
 }
