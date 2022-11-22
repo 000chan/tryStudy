875c57d35fdc923ca882792f379cdf2ecca190ec
Chung jae <89565071+knm8643@users.noreply.github.com>
2022-11-22 16:11:50 +0900

    Update ox퀴즈.java

--- L/lv0/ox퀴즈.java
+++ L/lv0/ox퀴즈.java
@@ -21,3 +21,19 @@ public class ox퀴즈 {
 //        }
 //    }
 }
+
+// js버전(통과함)
+// function solution(quiz){
+//     var answer = [];
+//     var disAnswer = [];
+    
+//     for(var i=0; i<quiz.length; i++){
+//         disAnswer[i] = quiz[i].split(" ");
+//         if(disAnswer[i][1] == "+"){
+//             answer[i] = ((Number(disAnswer[i][0]) + Number(disAnswer[i][2]))==disAnswer[i][4])?"O":"X"
+//         } else {
+//             answer[i] = ((Number(disAnswer[i][0]) - Number(disAnswer[i][2]))==disAnswer[i][4])?"O":"X"
+//         }
+//     }
+//     return answer;
+// }
