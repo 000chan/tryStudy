dfdff0e2fab56f7300432fc0c7cfdebdce8556b9
이충재 <knm8643@nate.com>
2022-11-24 20:00:57 +0900

    [PGS] 오픈채팅방(비통과) / 아이언2 / 60분" -m "사이트 길어서 제외"

--- L/lv2/오픈채팅방.js
+++ L/lv2/오픈채팅방.js
@@ -1,31 +1,29 @@
-오픈채팅방 js
-
 function solution(record) {
     var answer = [];
     var name = [];
     var changeName = [];
     // console.log(record);
-    
+
     for(var i=0; i<record.length; i++){
         name[i] = record[i].split(" ");
 
         if(record[i].indexOf("Enter") != -1){
             answer[i] = name[i][1] + "/" + name[i][2] + "/님이 들어왔습니다."
-        }        
-        if(record[i].indexOf("Leave") != -1){           
-           for(var j=0; j<answer.length-1; j++){
-              if(answer[j].indexOf(name[i][1]) != -1){
-                  name[i] = answer[j].split("/");
-                  answer[i] = name[i][0] + "/" + name[i][1] + "/님이 나갔습니다."      
-              }
-           }
+        }
+        if(record[i].indexOf("Leave") != -1){
+            for(var j=0; j<answer.length-1; j++){
+                if(answer[j].indexOf(name[i][1]) != -1){
+                    name[i] = answer[j].split("/");
+                    answer[i] = name[i][0] + "/" + name[i][1] + "/님이 나갔습니다."
+                }
+            }
         }
         if(record[i].indexOf("Change") != -1){
             for(var q=0; q<answer.length-1; q++){
-               if(answer[q].indexOf(name[i][1]) != -1){
-                   changeName[q] = answer[q].split("/");
-                   answer[q] = changeName[q][0] + "/" + name[i][2] +"/"+ changeName[q][2]
-               }
+                if(answer[q].indexOf(name[i][1]) != -1){
+                    changeName[q] = answer[q].split("/");
+                    answer[q] = changeName[q][0] + "/" + name[i][2] +"/"+ changeName[q][2]
+                }
             }
         }
     }
@@ -36,7 +34,7 @@ function solution(record) {
                 name[k] = answer[answer.length-1].split("/");
                 // answer[k] = changeName[k][1] + changeName[k][2]
                 answer[k] = name[k][1] + changeName[k][2]
-            }else{          
+            }else{
                 answer[k] = changeName[k][1] + changeName[k][2]
             }
         }
