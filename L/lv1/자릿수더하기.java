b8358d1f69f912af6b7753e55524ff1a0b111a81
이충재 <knm8643@nate.com>
2022-11-26 19:17:46 +0900

    [PSG] 자릿수더하기 / Lv.1 / 18분

---
+++ L/lv1/자릿수더하기.java
@@ -0,0 +1,18 @@
+package lv1;
+
+public class 자릿수더하기 {
+//    import java.util.*;
+//
+//    public class Solution {
+//        public int solution(int n) {
+//            int n_length = (int)(Math.log10(n)+1);
+//            String n_string = Integer.toString(n);
+//            int answer = 0;
+//
+//            for(int i=0; i<n_length; i++){
+//                answer += n_string.charAt(i) - '0';
+//            }
+//            return answer;
+//        }
+//    }
+}
