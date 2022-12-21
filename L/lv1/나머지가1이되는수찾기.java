f8a5f25672a418c0c8a174ca71c7b97ee2e47a72
이충재 <knm8643@nate.com>
2022-12-21 19:51:43 +0900

    [PGS] 나머지가 1이 되는 수 찾기 / lv1 / 10분 /

---
+++ L/lv1/나머지가1이되는수찾기.java
@@ -0,0 +1,16 @@
+package lv1;
+
+public class 나머지가1이되는수찾기 {
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+            for(int i=1; i<n; i++){
+                if(n%i == 1) {
+                    answer = i;
+                    break;
+                }
+            }
+            return answer;
+        }
+    }
+}
