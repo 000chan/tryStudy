19b60c35d7c9d4d215d7b7d9238b665fd47ea9ce
이충재 <knm8643@nate.com>
2023-01-07 10:15:23 +0900

    [PGS] 올바른 괄호 / lv2 / 60분 /

---
+++ L/lv2/올바른괄호.java
@@ -0,0 +1,26 @@
+package lv2;
+
+public class 올바른괄호 {
+//    import java.util.Stack;
+//    class Solution {
+//        boolean solution(String s) {
+//            boolean answer = true;
+//            Stack<Character> stack = new Stack<>();
+//
+//            if(s.substring(0,1).equals(")")){
+//                return false;
+//            } else {
+//                for(int i=0; i<s.length(); i++){
+//                    if(s.charAt(i) == '(') {
+//                        stack.push(s.charAt(i));
+//                    } else {
+//                        if(stack.isEmpty()) return false;
+//                        else stack.pop();
+//                    }
+//                }
+//                answer = (stack.isEmpty())?true:false;
+//            }
+//            return answer;
+//        }
+//    }
+}
